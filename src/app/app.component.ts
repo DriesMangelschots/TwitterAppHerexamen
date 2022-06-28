3b07d6e52ef705c3242eab7f683ec83a5ab1e939
Dries Mangelschots <r0590700@student.thomasmore.be>
2022-06-28 22:15:41 +0200

    Navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { Platform } from '@ionic/angular';
+import {Component, Renderer2} from '@angular/core';
+
 
 
 @Component({
@@ -10,41 +10,50 @@ import { Platform } from '@ionic/angular';
 export class AppComponent {
 
   navigate: any;
-  constructor(
-    private platform: Platform,
-
-  ) {
+  constructor(private renderer: Renderer2) {
     this.sideMenu();
   }
+  // eslint-disable-next-line @typescript-eslint/member-ordering
+  darkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
 
+  // eslint-disable-next-line @typescript-eslint/member-ordering
+  iconMoon = true;
+  // eslint-disable-next-line @typescript-eslint/member-ordering
+  modeText ='Lichte mode';
   sideMenu() {
     this.navigate =
       [
         {
-          title : 'App',
+          title : 'Logout',
           url   : '/apps',
-          icon  : 'apps'
+          icon  : 'log-out'
         },
-        {
-          title : 'Book',
-          url   : '/book',
-          icon  : 'book'
-        },
-        {
-          title : 'Paint',
-          url   : '/paint',
-          icon  : 'brush'
-        },
-        {
-          title : 'Contacts',
-          url   : '/contacts',
-          icon  : 'contacts'
-        },
-        {
-          title : 'Facebook',
-          url   : '/facebook.com',
-          icon  : 'logo-facebook'
-        },
+
+
       ];
   }
+  toggleDarkMode = () => {
+    this.changeNameMode();
+    if (this.darkMode) {
+      this.renderer.addClass(document.body,'dark');
+      console.log('enable darkmode');
+    } else {
+      this.renderer.removeClass(document.body,'dark');
+      console.log('enable lightmode');
+    }
+  };
+  toggleIcon = (): void => {
+    this.darkMode = !this.darkMode;
+    this.toggleDarkMode();
+    this.iconMoon = !this.iconMoon;
+
+
+  };
+  changeNameMode= () => {
+    if (this.darkMode) {
+      this.modeText = 'Donkere mode';
+    } else {
+      this.modeText = 'Lichte mode';
+    }
+  };
 }
